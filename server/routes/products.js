56119064b56cd5b20d575e05ec82ba3d09fcf75a
ibrar11 <ibrarmunawar0gmail.com>
2025-11-20 16:16:00 +0500

    api for removing product

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,5 +5,6 @@ const productsController = require('../app/http/controllers/products')
 router.get("/sortedProducts", productsController.getSortedProducts)
 router.get("/filteredProducts", productsController.getFilteredProducts)
 router.post("/addProducts", productsController.addProducts)
+router.delete("/:id", productsController.removeProducts)
 
 module.exports = router;

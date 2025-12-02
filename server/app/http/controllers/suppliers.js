471cbd349037c2d27d8fb297031d8c92c199d949
ibrar11 <ibrarmunawar0gmail.com>
2025-12-02 13:22:48 +0500

    api for get suppliers data

--- server/app/http/controllers/suppliers.js
+++ server/app/http/controllers/suppliers.js
@@ -46,6 +46,41 @@ const addSupplierData = async (req, res) => {
     }
 }
 
+const getSupplierData = async (req, res) => {
+    try {
+        const topFiveSuppliers = await models.sequelize.query(
+            `
+                SELECT
+                    s."id" as "SupplierID", 
+                    s."SupplierName", 
+                    SUM(d."Quantity" * p."Price") as "totalRevenue" 
+                FROM "Suppliers" s 
+                JOIN "Products" p 
+                    ON s."id" = p."SupplierId" 
+                JOIN "OrderDetails" d 
+                    ON d."ProductID" = p."id" 
+                GROUP BY s."id" 
+                ORDER BY "totalRevenue" DESC 
+                LIMIT 5;
+            `
+        )
+        if(topFiveSuppliers?.length === 0) {
+            return res.status(404).send({
+                messsage: "Suppliers not found."
+            })
+        }
+        return res.status(200).send({
+            topFiveSuppliers
+        })
+    } catch (err) {
+        console.log("Error getting suppliers ", err)
+        return res.status(500).send({
+            messsage: err.messsage,
+        })
+    }
+}
+
 module.exports = {
     addSupplierData,
+    getSupplierData
 }

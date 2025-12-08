59caa1882188ae91306d0bb3b64f836035597a72
ibrar11 <ibrarmunawar0gmail.com>
2025-12-08 16:41:02 +0500

    top selling product by revenue per category filtered along percentage

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const categoriesController = require("../app/http//controllers/categories")
 
+
+router.get("/",categoriesController.getCategoriesData)
 router.post("/",categoriesController.addCategoriesData)
 
 module.exports = router;
